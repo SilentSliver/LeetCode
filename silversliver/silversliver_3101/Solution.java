1f27dbeade9093625f0026b3f5633b535165942d
SilverSliver <j1617282370s@hotamil.com>
2024-07-07 04:00:48 +0800

    Submit 3033

--- silversliver/silversliver_3101/Solution.java
+++ silversliver/silversliver_3101/Solution.java
@@ -7,7 +7,12 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long countAlternatingSubarrays(int[] nums) {
-
+        long ans = 1, s = 1;
+        for (int i = 1; i < nums.length; i++) {
+            s = nums[i] != nums[i - 1] ? s + 1 : 1;
+            ans += s;
+        }
+        return ans;
     }
 
     @Override
