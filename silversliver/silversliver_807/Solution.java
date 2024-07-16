5c4f31255edbc2ab3e678120b69153b26c4196ea
SilverSliver <j1617282370s@hotamil.com>
2024-07-16 23:14:46 +0800

    Submit 721 and 807

--- silversliver/silversliver_807/Solution.java
+++ silversliver/silversliver_807/Solution.java
@@ -7,7 +7,19 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-
+        var ans = 0;
+        var n = grid.length;
+        var max = new int[2][n];
+        for (var i = 0; i < n; i++)
+            for (var j = 0; j < n; j++){
+                max[0][i] = Math.max(max[0][i], grid[i][j]);
+                max[1][i] = Math.max(max[1][i], grid[j][i]);
+            }
+        for (var i = 0; i < n; i++)
+            for (var j = 0; j < n; j++)
+                if (max[0][i] >= grid[i][j] && max[1][j] >= grid[i][j])
+                    ans += Math.min(max[0][i], max[1][j]) - grid[i][j];
+        return ans;
     }
 
     @Override
