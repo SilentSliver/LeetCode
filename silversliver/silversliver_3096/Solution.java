7aaecff2a95cb8dbfa0b6cfd32c78b379fae16b1
GitHub Action <action@github.com>
2024-07-20 14:01:09 +0000

    [20240720] Add daily problem solution

--- silversliver/silversliver_3096/Solution.java
+++ silversliver/silversliver_3096/Solution.java
@@ -7,7 +7,18 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int minimumLevels(int[] possible) {
-
+        var l_sum = 0;
+        var r_sum = 0;
+        var n = possible.length;
+        for (var i = 0; i < n; i++)
+            r_sum += (possible[i] == 1? 1: -1);
+        for (var i = 0; i < n - 1; i++) {
+            var tmp = (possible[i] == 1? 1: -1);
+            l_sum += tmp;
+            r_sum -= tmp;
+            if (l_sum > r_sum) return i + 1;
+        }
+        return -1;
     }
 
     @Override
