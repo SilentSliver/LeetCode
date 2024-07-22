f01f3fac10ed61578e869c86467a20d54872a2b4
SilverSliver <j1617282370s@hotamil.com>
2024-07-22 21:54:27 +0800

    Commit

--- silversliver/silversliver_1186/Solution.java
+++ silversliver/silversliver_1186/Solution.java
@@ -7,7 +7,22 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maximumSum(int[] arr) {
-
+        var n = arr.length;
+        var left = new int[n];
+        var right = new int[n];
+        var ans = Integer.MIN_VALUE;
+        for (int i = 0, s = 0; i < n; ++i) {
+            s = Math.max(s, 0) + arr[i];
+            left[i] = s;
+            ans = Math.max(ans, left[i]);
+        }
+        for (int i = n - 1, s = 0; i >= 0; --i) {
+            s = Math.max(s, 0) + arr[i];
+            right[i] = s;
+        }
+        for (var i = 1; i < n - 1; ++i)
+            ans = Math.max(ans, left[i - 1] + right[i + 1]);
+        return ans;
     }
 
     @Override
