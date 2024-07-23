cbc986a616a055385208ca3556ee688bab88c9f0
SilverSliver <j1617282370s@hotamil.com>
2024-07-23 21:49:37 +0800

    Commit

--- silversliver/silversliver_2101/Solution.java
+++ silversliver/silversliver_2101/Solution.java
@@ -7,7 +7,42 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        List<Integer>[] g = new List[n];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = i + 1; j < n; j++) {
+                int[] p1 = bombs[i], p2 = bombs[j];
+                var dist = Math.hypot(p1[0] - p2[0], p1[1] - p2[1]);
+                if (dist <= p1[2])
+                    g[i].add(j);
+                if (dist <= p2[2])
+                    g[j].add(i);
+            }
+        }
+        var ans = 0;
+        var vis = new boolean[n];
+        for (var k = 0; k < n; k++) {
+            Arrays.fill(vis, false);
+            vis[k] = true;
+            var cnt = 0;
+            var q = new ArrayDeque<Integer>();
+            q.offer(k);
+            while (!q.isEmpty()) {
+                var i = q.poll();
+                cnt++;
+                for (int j : g[i])
+                    if (!vis[j]) {
+                        vis[j] = true;
+                        q.offer(j);
+                    }
 
+            }
+            if (cnt == n)
+                return n;
+            ans = Math.max(ans, cnt);
+        }
+        return ans;
     }
 
     @Override
