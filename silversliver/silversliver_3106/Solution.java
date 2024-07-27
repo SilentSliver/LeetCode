706ed71afeeae03d8bb5492b8ee004a8c589febd
GitHub Action <action@github.com>
2024-07-27 14:01:32 +0000

    [20240727] Add daily problem solution

--- silversliver/silversliver_3106/Solution.java
+++ silversliver/silversliver_3106/Solution.java
@@ -6,9 +6,25 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public String getSmallestString(String s, int k) {
 
-    }
+        public String getSmallestString(String s, int k) {
+            var cs = s.toCharArray();
+            for (var i = 0; i < cs.length; ++i) {
+                var c1 = cs[i];
+                for (var c2 = 'a'; c2 < c1; ++c2) {
+                    var d = Math.min(c1 - c2, 26 - c1 + c2);
+                    if (d <= k) {
+                        cs[i] = c2;
+                        k -= d;
+                        break;
+                    }
+                }
+            }
+            return new String(cs);
+        }
+
+
+
 
     @Override
     public Object solve(String[] inputJsonValues) {
