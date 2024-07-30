07e64882e16e550d21d1088dcbf02666c99b7a59
GitHub Action <action@github.com>
2024-07-30 14:01:13 +0000

    [20240730] Add daily problem solution

--- silversliver/silversliver_2961/Solution.java
+++ silversliver/silversliver_2961/Solution.java
@@ -6,9 +6,29 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public List<Integer> getGoodIndices(int[][] variables, int target) {
 
+    public List<Integer> getGoodIndices(int[][] variables, int target) {
+        List<Integer> ans = new ArrayList<>();
+        for (var i = 0; i < variables.length; ++i) {
+            var e = variables[i];
+            int a = e[0], b = e[1], c = e[2], m = e[3];
+            if (qpow(qpow(a, b, 10), c, m) == target)
+                ans.add(i);
+
+        }
+        return ans;
     }
+
+    private int qpow(long a, int n, int mod) {
+        var ans = 1L;
+        for (; n > 0; n >>= 1) {
+            if ((n & 1) == 1)
+                ans = ans * a % mod;
+            a = a * a % mod;
+        }
+        return (int) ans;
+    }
+
 
     @Override
     public Object solve(String[] inputJsonValues) {
