f6642e5fd39692c3df5e721d9eba93b4ec3c249a
GitHub Action <action@github.com>
2024-08-03 14:01:47 +0000

    [20240803] Add daily problem solution

--- silversliver/silversliver_3128/Solution.java
+++ silversliver/silversliver_3128/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long numberOfRightTriangles(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        var rows = new int[m];
+        var cols = new int[n];
+        for (var i = 0; i < m; ++i) 
+            for (var j = 0; j < n; ++j) {
+                rows[i] += grid[i][j];
+                cols[j] += grid[i][j];
+            }
+        var ans = 0L;
+        for (var i = 0; i < m; ++i) 
+            for (var j = 0; j < n; ++j) 
+                if (grid[i][j] == 1) 
+                    ans += (rows[i] - 1) * (cols[j] - 1);
 
+        return ans;
     }
 
     @Override
--- silversliver/silversliver_3143/Solution.java
+++ silversliver/silversliver_3143/Solution.java
@@ -6,9 +6,39 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int maxPointsInsideSquare(int[][] points, String s) {
+    private int ans;
 
+    public int maxPointsInsideSquare(int[][] points, String str) {
+        char[] s = str.toCharArray();
+        int left = -1;
+        int right = 1_000_000_001;
+        while (left + 1 < right) {
+            int mid = (left + right) >>> 1;
+            if (check(mid, points, s))
+                left = mid;
+            else
+                right = mid;
+        }
+        return ans;
     }
+
+    boolean check(int size, int[][] points, char[] s) {
+        int vis = 0;
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            int c = s[i] - 'a';
+            if (Math.abs(x) <= size && Math.abs(y) <= size) {
+                if ((vis >> c & 1) > 0)
+                    return false;
+                vis |= 1 << c;
+            }
+        }
+        ans = Integer.bitCount(vis);
+        return true;
+    }
+
+
 
     @Override
     public Object solve(String[] inputJsonValues) {
