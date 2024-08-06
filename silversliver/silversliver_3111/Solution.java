19bd7db654c42831253e1c6f8957654881dbc6d9
SilverSliver <j1617282370s@hotamil.com>
2024-08-06 08:32:06 +0800

    Commit

--- silversliver/silversliver_3111/Solution.java
+++ silversliver/silversliver_3111/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int minRectanglesToCoverPoints(int[][] points, int w) {
-
+        Arrays.sort(points, (a, b) -> a[0] - b[0]);
+        int ans = 0, x1 = -1;
+        for (int[] p : points) {
+            var x = p[0];
+            if (x > x1) {
+                ++ans;
+                x1 = x + w;
+            }
+        }
+        return ans;
     }
 
     @Override
--- silversliver/silversliver_572/Solution.java
+++ silversliver/silversliver_572/Solution.java
@@ -22,7 +22,16 @@ import qubhjava.models.TreeNode;
 
 public class Solution extends BaseSolution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return false;
+        return dfs(root, subRoot) || isSubtree(root.left, subRoot)
+                || isSubtree(root.right, subRoot);
+    }
 
+    private boolean dfs(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return p == q;
+        return p.val == q.val && dfs(p.left, q.left) && dfs(p.right, q.right);
     }
 
     @Override
