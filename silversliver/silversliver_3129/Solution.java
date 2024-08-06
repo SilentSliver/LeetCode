29c96f3a5e931dee103ff138bbda37c36a8196b3
GitHub Action <action@github.com>
2024-08-06 14:01:17 +0000

    [20240806] Add daily problem solution

--- silversliver/silversliver_3129/Solution.java
+++ silversliver/silversliver_3129/Solution.java
@@ -7,8 +7,25 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int numberOfStableArrays(int zero, int one, int limit) {
+        final int MOD = 1_000_000_007;
+        int[][][] f = new int[zero + 1][one + 1][2];
+        for (int i = 1; i <= Math.min(limit, zero); i++)
+            f[i][0][0] = 1;
 
+        for (int j = 1; j <= Math.min(limit, one); j++)
+            f[0][j][1] = 1;
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                // + MOD 保证答案非负
+                f[i][j][0] = (int) (((long) f[i - 1][j][0] + f[i - 1][j][1] + (i > limit ? MOD - f[i - limit - 1][j][1] : 0)) % MOD);
+                f[i][j][1] = (int) (((long) f[i][j - 1][0] + f[i][j - 1][1] + (j > limit ? MOD - f[i][j - limit - 1][0] : 0)) % MOD);
+            }
+        }
+        return (f[zero][one][0] + f[zero][one][1]) % MOD;
     }
+
+
 
     @Override
     public Object solve(String[] inputJsonValues) {
