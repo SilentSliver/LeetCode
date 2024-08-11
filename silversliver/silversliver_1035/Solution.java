22adf314ee82bb32c99480e62e883931b790bf3f
GitHub Action <action@github.com>
2024-08-11 14:01:33 +0000

    [20240811] Add daily problem solution

--- silversliver/silversliver_1035/Solution.java
+++ silversliver/silversliver_1035/Solution.java
@@ -6,9 +6,19 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-
+    public int maxUncrossedLines(int[] s1, int[] s2) {
+        int n = s1.length, m = s2.length;
+        var f = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++) {
+                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                if (s1[i - 1] == s2[j - 1])
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - 1] + 1);
+            }
+        return f[n][m];
     }
+
+
 
     @Override
     public Object solve(String[] inputJsonValues) {
