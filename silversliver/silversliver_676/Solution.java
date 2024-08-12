92f2367b4c94f2c614d4f98ef5757e07e4ad65e0
SilverSliver <j1617282370s@hotamil.com>
2024-08-12 22:04:55 +0800

    Commit

--- silversliver/silversliver_676/Solution.java
+++ silversliver/silversliver_676/Solution.java
@@ -6,20 +6,47 @@ import qubhjava.BaseSolution;
 
 
 class MagicDictionary {
+	static final int N = 100 * 100, M = 26;
+	int idx = 0;
+	int[][] trie = new int[N][M];
+	boolean[] isEnd = new boolean[N * M];
 
-    public MagicDictionary() {
+	void add(String cur) {
+		var p = 0;
+		for (var i = 0; i < cur.length(); i++) {
+			int u = cur.charAt(i) - 'a';
+			if (trie[p][u] == 0) trie[p][u] = ++idx;
+			p = trie[p][u];
+		}
+		isEnd[p] = true;
+	}
 
-    }
-    
-    public void buildDict(String[] dictionary) {
+	boolean query(String cur, int idx, int p, int limit) {
+		if (limit < 0) return false;
+		if (idx == cur.length()) return isEnd[p] && limit == 0;
+		var u = cur.charAt(idx) - 'a';
+		for (var i = 0; i < 26; i++) {
+			if (trie[p][i] == 0) continue;
+			if (query(cur, idx + 1, trie[p][i], i == u ? limit : limit - 1)) return true;
+		}
+		return false;
+	}
 
-    }
-    
-    public boolean search(String searchWord) {
+	public void buildDict(String[] dictionary) {
+		for (var cur: dictionary) add(cur);
+	}
 
-    }
+	public boolean search(String searchWord) {
+		return query(searchWord, 0, 0, 1);
+	}
 }
 
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dictionary);
+ * boolean param_2 = obj.search(searchWord);
+ */
 /**
  * Your MagicDictionary object will be instantiated and called as such:
  * MagicDictionary obj = new MagicDictionary();
