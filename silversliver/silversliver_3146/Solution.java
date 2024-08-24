cdaba498bea84accfed6dbc87d0ac85841946ca4
GitHub Action <action@github.com>
2024-08-24 14:01:45 +0000

    [20240824] Add daily problem solution

--- silversliver/silversliver_3146/Solution.java
+++ silversliver/silversliver_3146/Solution.java
@@ -7,7 +7,15 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int findPermutationDifference(String s, String t) {
-
+        int[] pos = new int[26];
+        for (int i = 0; i < s.length(); i++) 
+            pos[s.charAt(i) - 'a'] = i;
+        
+        int ans = 0;
+        for (int i = 0; i < t.length(); i++) 
+            ans += Math.abs(i - pos[t.charAt(i) - 'a']);
+        
+        return ans;
     }
 
     @Override
