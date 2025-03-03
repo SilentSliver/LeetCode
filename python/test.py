02fbddb5d40503cdad2d3b8ad240fac1557320fd
GitHub Action <action@github.com>
2025-03-03 16:18:30 +0000

    test: [20250304] Add daily LeetCode problem

--- python/test.py
+++ python/test.py
@@ -11,7 +11,7 @@ from utils import get_default_folder, timeout
 logging.basicConfig(level=logging.INFO, format=constants.LOGGING_FORMAT, datefmt=constants.DATE_FORMAT)
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "1278"
+QUESTION = "1745"
 
 
 class Test(unittest.TestCase):
