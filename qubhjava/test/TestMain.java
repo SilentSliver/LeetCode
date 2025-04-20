e02185fc1a47254552fe3b801663f877f9fd7fe9
GitHub Action <action@github.com>
2025-04-20 16:17:28 +0000

    test: [20250421] Add daily LeetCode problem

--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.util.Strings;
-import silversliver.silversliver_781.Solution;
+import silversliver.silversliver_2145.Solution;
 import qubhjava.Testcase;
 
 import java.io.IOException;
@@ -21,7 +21,7 @@ import java.util.List;
 public class TestMain {
 
     private static final Logger log = LoggerFactory.getLogger(TestMain.class);
-    private static final String PROBLEM_ID = "781";
+    private static final String PROBLEM_ID = "2145";
 
     private Testcase[] loadTestcases() throws IOException {
         String problemFolder = null;
