fbc6ff041e54be2eb7a398299ce5ebe3fcb743e5
SilverSliver <j1617282370s@hotamil.com>
2025-04-23 23:17:11 +0800

    Commit 1399

--- silversliver/silversliver_1399/Solution.java
+++ silversliver/silversliver_1399/Solution.java
@@ -1,13 +1,27 @@
 package silversliver.silversliver_1399;
 
 import com.alibaba.fastjson.JSON;
-import java.util.*;
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public int countLargestGroup(int n) {
-        
+        var cnt = new int[40];
+        int ans = 0, cur_max = 0;
+        for (var i = 1; i <= n; ++i) {
+            var sum = 0;
+            for (var x = i; x > 0; x /= 10)
+                sum += x % 10;
+
+            ++cnt[sum];
+            if (cur_max < cnt[sum]) {
+                cur_max = cnt[sum];
+                ans = 1;
+            } else if (cur_max == cnt[sum]) {
+                ++ans;
+            }
+        }
+        return ans;
     }
 
     @Override
--- silversliver/silversliver_2109/Solution.java
+++ silversliver/silversliver_2109/Solution.java
@@ -7,7 +7,11 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public String addSpaces(String s, int[] spaces) {
-        
+        var ans = new StringBuffer();
+        var space_idx = 0;
+        for (int i = 0; i < s.length(); i++)
+            ans.append(space_idx < spaces.length && i == spaces[space_idx] ? " " + s.charAt(spaces[space_idx++]) : s.charAt(i));
+        return ans.toString();
     }
 
     @Override
