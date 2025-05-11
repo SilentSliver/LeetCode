18faa5c22c64b25da68e88098b7a0fc619c3d6f3
GitHub Action <action@github.com>
2025-05-11 16:18:17 +0000

    test: [20250512] Add (2094)

--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 // import problems.problems_3342.Solution;
 
 import org.testng.util.Strings;
-import silversliver.silversliver_1550.Solution;
+import silversliver.silversliver_2094.Solution;
 import qubhjava.Testcase;
 
 import java.io.IOException;
@@ -23,7 +23,7 @@ import java.util.List;
 public class TestMain {
 
     private static final Logger log = LoggerFactory.getLogger(TestMain.class);
-    private static final String PROBLEM_ID = "1550";
+    private static final String PROBLEM_ID = "2094";
 
     private Testcase[] loadTestcases() throws IOException {
         String problemFolder = null;
