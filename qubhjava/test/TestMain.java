ad5b66690e58b7389210422b5313ddd5b4c7a5be
SilverSliver <j1617282370s@hotamil.com>
2025-05-13 22:50:13 +0800

    Commit 3335

--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import problems.problems_3335.Solution;
+// import problems.problems_3335.Solution;
 
 
 import org.testng.util.Strings;
--- silversliver/silversliver_3335/Solution.java
+++ silversliver/silversliver_3335/Solution.java
@@ -6,8 +6,25 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    static final int mod = (int) 1e9 + 7;
+
     public int lengthAfterTransformations(String s, int t) {
-        
+        var f = new int[t + 1][26];
+        for (var c : s.toCharArray()) {
+            f[0][c - 'a']++;
+        }
+        for (var i = 1; i <= t; ++i) {
+            f[i][0] = f[i - 1][25] % mod;
+            f[i][1] = (f[i - 1][0] + f[i - 1][25]) % mod;
+            for (var j = 2; j < 26; j++)
+                f[i][j] = f[i - 1][j - 1] % mod;
+        }
+
+        var ans = 0;
+        for (var j = 0; j < 26; ++j) {
+            ans = (ans + f[t][j]) % mod;
+        }
+        return ans;
     }
 
     @Override
