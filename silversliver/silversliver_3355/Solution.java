fcbc53b3e7be1ca9f00d8abe1b95800a83cd7883
SilverSliver <j1617282370s@hotamil.com>
2025-05-20 23:09:44 +0800

    Commit 3355

--- silversliver/silversliver_3355/Solution.java
+++ silversliver/silversliver_3355/Solution.java
@@ -7,7 +7,19 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        
+        var n = nums.length;
+        var d = new int[n + 1];
+        for (var q : queries) {
+            int l = q[0], r = q[1];
+            ++d[l];
+            --d[r + 1];
+        }
+        for (int i = 0, s = 0; i < n; i++) {
+            s += d[i];
+            if (nums[i] > s)
+                return false;
+        }
+        return true;
     }
 
     @Override
