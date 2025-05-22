b6455fc0e3daea292cdbe749c8dbd97b616bcea4
SilverSliver <j1617282370s@hotamil.com>
2025-05-22 22:58:41 +0800

    Commit 3362

--- silversliver/silversliver_3362/Solution.java
+++ silversliver/silversliver_3362/Solution.java
@@ -7,7 +7,27 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maxRemoval(int[] nums, int[][] queries) {
-        
+//        对queries数组排列，按l下标从小到大排列
+        Arrays.sort(queries,
+                Comparator.comparingInt(a -> a[0]));
+        var pq = new PriorityQueue<Integer>((a, b) -> b - a);
+        var n = nums.length;
+        var d = new int[n + 1];
+        int s = 0, j = 0;
+        for (var i = 0; i < n; i++) {
+            s += d[i];
+            while (j < queries.length && queries[j][0] <= i) {
+                pq.offer(queries[j][1]);
+                j++;
+            }
+            while (s < nums[i] && !pq.isEmpty() && pq.peek() >= i) {
+                s++;
+                d[pq.poll() + 1]--;
+            }
+            if (s < nums[i])
+                return -1;
+        }
+        return pq.size();
     }
 
     @Override
