41c28cfde851b3d75b3d934aca959dcc7373358c
SilverSliver <j1617282370s@hotamil.com>
2025-05-27 22:46:41 +0800

    Commit 2894

--- silversliver/silversliver_2894/Solution.java
+++ silversliver/silversliver_2894/Solution.java
@@ -2,12 +2,14 @@ package silversliver.silversliver_2894;
 
 import com.alibaba.fastjson.JSON;
 import java.util.*;
+import java.util.stream.IntStream;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public int differenceOfSums(int n, int m) {
-        
+        return n * (n + 1) / 2 - n / m * (n / m + 1) * m;
     }
 
     @Override
