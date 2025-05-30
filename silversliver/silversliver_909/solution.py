9207d1cbdc10944cb1095696e617ea450f52d850
GitHub Action <action@github.com>
2025-05-30 16:20:55 +0000

    test: [20250531] Add (909)

---
+++ silversliver/silversliver_909/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.snakesAndLadders(test_input)
+
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        pass
+
