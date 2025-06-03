23bc102b96d944c3b2cde010c9357d55eb34135d
SilverSliver <j1617282370s@hotamil.com>
2025-06-03 20:50:45 +0800

    Commit 1298

--- silversliver/silversliver_1298/Solution.java
+++ silversliver/silversliver_1298/Solution.java
@@ -6,8 +6,43 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private ArrayDeque<Integer> cur_box_que;
+    private int[] g_candies;
+    private int[] used;
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-        
+        cur_box_que = new ArrayDeque<Integer>();
+        g_candies = candies;
+        used = new int[status.length];
+        var ans = 0;
+        for (var cur_box_id: initialBoxes){
+            used[cur_box_id] = 1;
+            if (status[cur_box_id] == 1)
+                ans += getCandies(cur_box_id);
+        }
+        while (!cur_box_que.isEmpty()) {
+            var cur_box_id = cur_box_que.poll();
+            for (var key: keys[cur_box_id])
+                if (status[key] == 0) {
+                    status[key] = 1;
+                    if (used[key] == 1)
+                        ans += getCandies(key);
+
+                }
+
+            for (var con_box_id : containedBoxes[cur_box_id]) {
+                used[con_box_id] = 1;
+                if (status[con_box_id] == 1 && used[con_box_id] == 1)
+                    ans += getCandies(con_box_id);
+
+            }
+        }
+        return ans;
+    }
+
+    private int getCandies(int cur_box_id){
+        cur_box_que.offer(cur_box_id);
+        used[cur_box_id] = 2;
+        return g_candies[cur_box_id];
     }
 
     @Override
