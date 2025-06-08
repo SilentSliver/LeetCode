78306b885b1ce9a6e059d0e0eeba3045ee26344f
GitHub Action <action@github.com>
2025-06-08 16:18:55 +0000

    test: [20250609] Add (440)

--- python/test.py
+++ python/test.py
@@ -11,7 +11,7 @@ from utils import get_default_folder, timeout
 logging.basicConfig(level=logging.INFO, format=constants.LOGGING_FORMAT, datefmt=constants.DATE_FORMAT)
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "386"
+QUESTION = "440"
 
 
 class Test(unittest.TestCase):
