a7e0a967be0169c7162fa138d57bf9dbc168dcb1
SilverSliver <j1617282370s@hotamil.com>
2025-06-10 21:01:06 +0800

    Commit 3422

--- silversliver/silversliver_3442/Solution.java
+++ silversliver/silversliver_3442/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maxDifference(String s) {
-        
+        var cnt = new int[26];
+        for (var c : s.toCharArray()) cnt[c - 'a']++;
+        var a1 = Integer.MIN_VALUE;
+        var a2 = Integer.MAX_VALUE;
+        for (var val : cnt)
+            if (val % 2 == 1)
+                a1 = Math.max(a1, val);
+            else if (val > 0)
+                a2 = Math.min(a2, val);
+        return a1 - a2;
     }
 
     @Override
