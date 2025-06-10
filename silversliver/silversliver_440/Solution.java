4d1b8ddb37fbd50b0b30e75ca106d071922dde43
SilverSliver <j1617282370s@hotamil.com>
2025-06-10 20:53:23 +0800

    Commit 440

--- silversliver/silversliver_440/Solution.java
+++ silversliver/silversliver_440/Solution.java
@@ -1,13 +1,40 @@
 package silversliver.silversliver_440;
 
 import com.alibaba.fastjson.JSON;
-import java.util.*;
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private int len;
+
     public int findKthNumber(int n, int k) {
-        
+        len = n;
+        var cur = 1L;
+        // 当前剩余位数
+        --k;
+        while (k > 0) {
+            // 步进策略, 根据当前位
+            var cnt = count(cur);
+            if (k >= cnt) {
+                k -= cnt;
+                ++cur;
+            } else {
+                --k;
+                cur *= 10;
+            }
+        }
+        return (int) cur;
+    }
+
+    public int count(long cur) {
+        var next = cur + 1L;
+        var cnt = 0L;
+        while (cur <= len) {
+            cnt += Math.min(len - cur + 1, next - cur);
+            next *= 10;
+            cur *= 10;
+        }
+        return (int) cnt;
     }
 
     @Override
