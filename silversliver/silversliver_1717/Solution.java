56dc92e8e235d8aeef003c3b6e19aafff271ba61
GitHub Action <action@github.com>
2025-07-22 16:22:55 +0000

    test: [20250723] Add (1717)

---
+++ silversliver/silversliver_1717/Solution.java
@@ -0,0 +1,20 @@
+package silversliver.silversliver_1717;
+
+import com.alibaba.fastjson.JSON;
+import java.util.*;
+import qubhjava.BaseSolution;
+
+
+public class Solution extends BaseSolution {
+    public int maximumGain(String s, int x, int y) {
+        
+    }
+
+    @Override
+    public Object solve(String[] inputJsonValues) {
+        String s = jsonStringToString(inputJsonValues[0]);
+		int x = Integer.parseInt(inputJsonValues[1]);
+		int y = Integer.parseInt(inputJsonValues[2]);
+        return JSON.toJSON(maximumGain(s, x, y));
+    }
+}
