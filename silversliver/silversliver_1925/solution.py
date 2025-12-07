8137acbcc3d18cc008a6847eac1145da1e000e59
GitHub Action <action@github.com>
2025-12-07 16:19:28 +0000

    test: [20251208] Add (1925)

---
+++ silversliver/silversliver_1925/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.countTriples(test_input)
+
+    def countTriples(self, n: int) -> int:
+        pass
+
