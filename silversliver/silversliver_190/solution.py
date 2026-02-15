e968569863f335ef1ce144afb3d8ec9e17796bb8
GitHub Action <action@github.com>
2026-02-15 16:26:46 +0000

    test: [20260216] Add (190)

---
+++ silversliver/silversliver_190/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.reverseBits(test_input)
+
+    def reverseBits(self, n: int) -> int:
+        pass
+
