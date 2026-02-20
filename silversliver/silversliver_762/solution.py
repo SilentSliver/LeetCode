a824116be05fde19f26ea27cfd46dcdef7c6d818
GitHub Action <action@github.com>
2026-02-20 16:35:35 +0000

    test: [20260221] Add (762)

---
+++ silversliver/silversliver_762/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.countPrimeSetBits(*test_input)
+
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        pass
+
