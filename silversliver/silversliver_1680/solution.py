8fa299b79e14f43c56f9ffc44866e8a6a8aac0d7
GitHub Action <action@github.com>
2026-02-27 16:37:40 +0000

    test: [20260228] Add (1680)

---
+++ silversliver/silversliver_1680/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.concatenatedBinary(test_input)
+
+    def concatenatedBinary(self, n: int) -> int:
+        pass
+
