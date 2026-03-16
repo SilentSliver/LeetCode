c6c6110bf75f4bd4870bc47e5d19336a75e5352d
GitHub Action <action@github.com>
2026-03-16 16:58:30 +0000

    test: [20260317] Add (1727)

---
+++ silversliver/silversliver_1727/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.largestSubmatrix(test_input)
+
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        pass
+
