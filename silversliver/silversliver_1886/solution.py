c7cd5801424ac7257d7f25fa8e23653f7b044240
GitHub Action <action@github.com>
2026-03-21 16:26:15 +0000

    test: [20260322] Add (1886)

---
+++ silversliver/silversliver_1886/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.findRotation(*test_input)
+
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        pass
+
