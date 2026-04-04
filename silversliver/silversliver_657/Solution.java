6c26b745b329970422af1fd29ae83d398d976c0c
GitHub Action <action@github.com>
2026-04-04 16:32:06 +0000

    test: [20260405] Add (657)

---
+++ silversliver/silversliver_657/Solution.java
@@ -0,0 +1,18 @@
+package silversliver.silversliver_657;
+
+import com.alibaba.fastjson.JSON;
+import java.util.*;
+import qubhjava.BaseSolution;
+
+
+public class Solution extends BaseSolution {
+    public boolean judgeCircle(String moves) {
+        
+    }
+
+    @Override
+    public Object solve(String[] inputJsonValues) {
+        String moves = jsonStringToString(inputJsonValues[0]);
+        return JSON.toJSON(judgeCircle(moves));
+    }
+}
