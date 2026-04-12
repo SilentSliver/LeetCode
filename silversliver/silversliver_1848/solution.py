bc6d41d20861d4904efab5087a476845a169dfd5
GitHub Action <action@github.com>
2026-04-12 16:35:36 +0000

    test: [20260413] Add (1848)

---
+++ silversliver/silversliver_1848/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.getMinDistance(*test_input)
+
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        pass
+
