8488d232d3032ddbc0a7621b98807e4ea016fc96
GitHub Action <action@github.com>
2026-04-20 17:01:05 +0000

    test: [20260421] Add (1722)

---
+++ silversliver/silversliver_1722/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.minimumHammingDistance(*test_input)
+
+    def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
+        pass
+
