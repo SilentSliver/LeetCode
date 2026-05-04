2256e27c24502d3835cd6759e615b399d6cd70f3
GitHub Action <action@github.com>
2026-05-04 17:27:22 +0000

    test: [20260505] Add (61)

---
+++ silversliver/silversliver_61/solution.py
@@ -0,0 +1,21 @@
+import solution
+from typing import *
+from python.object_libs import list_to_linked_list, linked_list_to_list
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        nums0, k = test_input
+        head0 = list_to_linked_list(nums0)
+        res = self.rotateRight(head0, k)
+        return linked_list_to_list(res)
+
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        pass
+
