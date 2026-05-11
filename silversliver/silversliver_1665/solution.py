b4d909ac6a62860858f54568259d66e7a668316b
GitHub Action <action@github.com>
2026-05-11 17:52:06 +0000

    test: [20260512] Add (1665)

---
+++ silversliver/silversliver_1665/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.minimumEffort(test_input)
+
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        pass
+
