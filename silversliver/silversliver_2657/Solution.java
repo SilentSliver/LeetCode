a82eff213c705d402db1218a096b7b7e40344974
GitHub Action <action@github.com>
2026-05-19 18:02:38 +0000

    test: [20260520] Add (2657)

---
+++ silversliver/silversliver_2657/Solution.java
@@ -0,0 +1,19 @@
+package silversliver.silversliver_2657;
+
+import com.alibaba.fastjson.JSON;
+import java.util.*;
+import qubhjava.BaseSolution;
+
+
+public class Solution extends BaseSolution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        
+    }
+
+    @Override
+    public Object solve(String[] inputJsonValues) {
+        int[] A = jsonArrayToIntArray(inputJsonValues[0]);
+		int[] B = jsonArrayToIntArray(inputJsonValues[1]);
+        return JSON.toJSON(findThePrefixCommonArray(A, B));
+    }
+}
