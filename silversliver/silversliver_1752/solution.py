35f6f099bf0a8a7c756bd3ff79709fbaae6f9496
GitHub Action <action@github.com>
2026-05-22 17:48:38 +0000

    test: [20260523] Add (1752)

---
+++ silversliver/silversliver_1752/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.check(test_input)
+
+    def check(self, nums: List[int]) -> bool:
+        pass
+
