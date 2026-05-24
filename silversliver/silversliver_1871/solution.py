32fc80950e52848a88f4fddceaaf494336c59fbd
GitHub Action <action@github.com>
2026-05-24 16:57:06 +0000

    test: [20260525] Add (1871)

---
+++ silversliver/silversliver_1871/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.canReach(*test_input)
+
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        pass
+
