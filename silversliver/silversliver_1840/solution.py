1230b0aae072c05569d0043f99334fb48ad4e760
GitHub Action <action@github.com>
2026-06-19 17:53:16 +0000

    test: [20260620] Add (1840)

---
+++ silversliver/silversliver_1840/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.maxBuilding(*test_input)
+
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        pass
+
