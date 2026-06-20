74041f843961bc161ac2f2bbdbad101975f99e7a
GitHub Action <action@github.com>
2026-06-20 17:23:18 +0000

    test: [20260621] Add (1833)

---
+++ silversliver/silversliver_1833/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.maxIceCream(*test_input)
+
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        pass
+
