cb1071a8d33c3881b51337a02778d227523d192b
GitHub Action <action@github.com>
2026-06-28 17:06:01 +0000

    test: [20260629] Add (1967)

---
+++ silversliver/silversliver_1967/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.numOfStrings(*test_input)
+
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        pass
+
