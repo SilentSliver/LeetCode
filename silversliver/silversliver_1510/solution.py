7f31aa92a71320723619490cc1d19a916ea20db3
GitHub Action <action@github.com>
2026-08-09 16:30:03 +0000

    test: [20260810] Add (1510)

---
+++ silversliver/silversliver_1510/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.winnerSquareGame(test_input)
+
+    def winnerSquareGame(self, n: int) -> bool:
+        pass
+
