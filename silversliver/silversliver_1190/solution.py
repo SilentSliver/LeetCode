cae58715ed93cad812dcc3a9be0978eaac42ef1e
GitHub Action <action@github.com>
2026-09-26 18:58:33 +0000

    test: [20260927] Add (1190)

---
+++ silversliver/silversliver_1190/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.reverseParentheses(test_input)
+
+    def reverseParentheses(self, s: str) -> str:
+        pass
+
---
+++ silversliver/silversliver_1190/testcase.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+import testcase
+
+case = namedtuple("Testcase", ["Input", "Output"])
+
+
+class Testcase(testcase.Testcase):
+	def __init__(self):
+		self.testcases = []
+		self.testcases.append(case(Input="(abcd)", Output="dcba"))
+		self.testcases.append(case(Input="(u(love)i)", Output="iloveu"))
+		self.testcases.append(case(Input="(ed(et(oc))el)", Output="leetcode"))
+
+	def get_testcases(self):
+		return self.testcases
